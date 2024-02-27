4f5a86bfce20c32caecc38747e3cf0e1db0abbe6
Camilla Scandola <103769428+camilla-scandola@users.noreply.github.com>
2024-02-27 13:51:15 +0100

    Update app.py

--- my_package/app.py
+++ my_package/app.py
@@ -1,1 +1,38 @@
+from flask import Flask, request, render_template, jsonify
+from functions import check_syllable
+from functions import check_word
+from functions import latin_to_hiragana
+from functions import hiragana_english
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('test_page.html')
+
+@app.route('/result_function1', methods=['POST'])
+def result_function1():
+    syllable = request.form.get('function1Input')
+    result = check_syllable(syllable)
+    return jsonify({'result': result})
+
+@app.route('/result_function2', methods=['POST'])
+def result_function2():
+    word = request.form.get('function2Input')
+    result = check_word(word)
+    return jsonify({'result': result})
+
+@app.route('/result_function3', methods=['POST'])
+def result_function3():
+    jpword = request.form.get('function3Input')
+    result = latin_to_hiragana(jpword)
+    return jsonify({'result': result})
+
+@app.route('/result_function4', methods=['POST'])
+def result_function4():
+    jpword = request.form.get('function4Input')
+    result = hiragana_english(jpword)
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
