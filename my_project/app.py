40e705c69392357d46442d5959d77463eb550af4
Camilla Scandola <103769428+camilla-scandola@users.noreply.github.com>
2024-03-06 18:50:58 +0100

    Update app.py

--- my_project/app.py
+++ my_project/app.py
@@ -2,37 +2,36 @@ from flask import Flask, request, render_template, jsonify
 from functions import check_syllable
 from functions import check_word
 from functions import latin_to_hiragana
-from functions import hiragana_english
+from functions import english_translation
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('test_page.html')
+    
+    result1 = ""
+    result2 = ""
+    result3 = ""
+    result4 = ""
 
-@app.route('/result_function1', methods=['POST'])
-def result_function1():
-    syllable = request.form.get('function1Input')
-    result = check_syllable(syllable)
-    return jsonify({'result': result})
+    if request.method == 'POST':
+        # Determine which function to call based on a hidden input in the form
+        action = request.form.get('action')
+        print(action)
+        if action == 'function1':
+            syllable = request.form.get('function1Input')
+            result1 = check_syllable(syllable)
+        elif action == 'function2':
+            word = request.form.get('function2Input')
+            result2 = check_word(word)
+        elif action == 'function3':
+            jpword = request.form.get('function3Input')
+            result3 = latin_to_hiragana(jpword)
+        elif action == 'function4':
+            jpword = request.form.get('function4Input')
+            result4 = english_translation(jpword)
 
-@app.route('/result_function2', methods=['POST'])
-def result_function2():
-    word = request.form.get('function2Input')
-    result = check_word(word)
-    return jsonify({'result': result})
-
-@app.route('/result_function3', methods=['POST'])
-def result_function3():
-    jpword = request.form.get('function3Input')
-    result = latin_to_hiragana(jpword)
-    return jsonify({'result': result})
-
-@app.route('/result_function4', methods=['POST'])
-def result_function4():
-    jpword = request.form.get('function4Input')
-    result = hiragana_english(jpword)
-    return jsonify({'result': result})
+    return render_template('index.html', result1=result1, result2=result2, result3=result3, result4=result4)
 
 if __name__ == '__main__':
     app.run(debug=True)
